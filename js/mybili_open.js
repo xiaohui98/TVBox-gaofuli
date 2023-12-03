c3d8b5c67f08ba31ba998525251b563e0ca9f14f
xiaohui98 <105145924+xiaohui98@users.noreply.github.com>
2023-12-03 21:43:40 +0800

    Update mybili_open.js

--- js/mybili_open.js
+++ js/mybili_open.js
@@ -6,7 +6,7 @@ let siteKey = '';
 let siteType = 0;
 const PC_UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.361";
 // let cookie = "DedeUserID=690781341;DedeUserID__ckMd5=cabc96906269c5b6;SESSDATA=2245ba24%2C1684212125%2C466fd%2Ab2;bili_jct=de6fdb60c10f8a83910aa55d79407b4e;"; // 可更换成自己的cookie
-let cookie = "DedeUserID=690781341;DedeUserID__ckMd5=cabc96906269c5b6;SESSDATA=2245ba24%2C1684212125%2C466fd%2Ab2;bili_jct=de6fdb60c10f8a83910aa55d79407b4e"; // 可更换成自己的cookie
+let cookie = "SESSDATA=cab46ba3%2C1715273592%2C83ee0%2Ab2CjCB_Y5DTJ94M5pjVVy2r72DugpUaKiwSJqE4NgrmEcwbqFVJjFxWPXUGC7maXIDB7sSVjJmU2xzbEhONEFwTlRVOG5TUlJsbjVhZFkwOFVwaTNvbWZDdXpRX3plYlBBVVN0R0ZpaU0zS2RCSzF3Z2hHS1RBZEtwWlRJR3lRTVptcnNPZEFNc0Z3IIEC;bili_jct=8e6f3a2caf1028328a2b378488f55a17;DedeUserID=400192087;DedeUserID__ckMd5=cc62ac2084d90a7d;sid=7g24p67f"; // 可更换成自己的cookie
 
 async function request(reqUrl) {
   const res = await req(reqUrl, {
